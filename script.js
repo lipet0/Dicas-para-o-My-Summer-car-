18acd4182a9ba3d505ace78a552a786d0ff217b6
lipet0 <lipetorressilva06@gmail.com>
2025-05-02 17:53:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 button.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme');
 });
+const whatsappButton = document.querySelector('.whatsapp-button');
+
+whatsappButton.addEventListener('click', function() {
+    const phoneNumber = '11967339208'; // Seu número
+    const message = encodeURIComponent('Olá, gostaria de saber mais informações!');
+    const url = `https://wa.me/${phoneNumber}?text=${message}`;
+    window.open(url, '_blank'); // Abre o link em uma nova janela
+});
