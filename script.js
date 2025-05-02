430c27d5df02f77cb1f9b73401f2ee2ca8aa56d0
lipet0 <lipetorressilva06@gmail.com>
2025-05-02 18:27:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ button.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme');
 });
 
-const whatsappButton = document.querySelector('.whatsapp-btn');  // Alterado de '.whatsapp-button' para '.whatsapp-btn'
+const whatsappButton = document.querySelector('.whatsapp-btn');
 
 whatsappButton.addEventListener('click', function() {
-  const phoneNumber = '5511967339208';
+    const phoneNumber = '5511967339208'; // Número com código do país
     const message = encodeURIComponent('Tire suas dúvidas comigo');
     const url = `https://wa.me/${phoneNumber}?text=${message}`;
     window.open(url, '_blank'); // Abre o link em uma nova janela
 });
-
